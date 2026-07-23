376e12392c0f69f7f8baf8f713f404a05e67ed55
drhkngvn-creator <drhkngvn@gmail.com>
2026-07-23 12:42:20 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.drhkngvn.cltiglobal"
+    namespace = "com.example.clti_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.drhkngvn.cltiglobal"
+        applicationId = "com.example.clti_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
